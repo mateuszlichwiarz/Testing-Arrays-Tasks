1d2bc0753665ad1bbe1687c206b04647e4804893
Mateusz Lichwiarz <mateusz.lichwiarz@gmail.com>
2020-05-25 19:43:55 +0200

    Few tests for user class

--- tests/unit/UserTest.php
+++ tests/unit/UserTest.php
@@ -13,4 +13,30 @@ class UserTest extends TestCase
 
         $this->assertEquals($user->getFirstName(), 'Mateusz');
     }
+
+    public function testThatWeCanGetTheLastName()
+    {
+        $user = new User();
+        $user->setLastName('Piotr');
+
+        $this->assertEquals($user->getLastName(), 'Piotr');
+    }
+
+    public function testFullNameIsReturned()
+    {
+        $user = new User();
+        $user->setFirstName('Mateusz');
+        $user->setLastName('Piotr');
+
+        $full_name = $user->getFullName();
+
+        $this->assertEquals($full_name, 'Mateusz Piotr');
+    }
+
+    public function testTrimmedString()
+    {
+        $trimmed_string = User::getTrimmedString('   Mateusz   ');
+
+        $this->assertEquals($trimmed_string, "Mateusz");
+    }
 }
