5fd9fe00c5f998d101c9d6c9a861e9c8021df21e
Mateusz Lichwiarz <mateusz.lichwiarz@gmail.com>
2020-05-26 15:14:55 +0200

    tests collection merged

--- tests/unit/CollectionTest.php
+++ tests/unit/CollectionTest.php
@@ -23,21 +23,61 @@ class CollectionTest extends TestCase
         $this->assertEquals(3, $collection->count());
     }
 
-     /** @test */
-     public function items_returned_match_items_passed_in()
-     {
-         $array = ['one','two'];
-         $collection = new Collection($array);
+    /** @test */
+    public function items_returned_match_items_passed_in()
+    {
+        $array = ['one','two'];
+        $collection = new Collection($array);
  
-         $this->assertCount(2, $collection->get());
-         $this->assertEquals($collection->get()[0], 'one');
-         $this->assertEquals($collection->get()[1], 'two');
-     }
+        $this->assertCount(2, $collection->get());
+        $this->assertEquals($collection->get()[0], 'one');
+        $this->assertEquals($collection->get()[1], 'two');
+    }
 
-     /** @test */
-     public function collection_is_instance_of_iterator_aggregate()
-     {
-         $collection = new Collection();
-         $this->assertInstanceOf(IteratorAggregate::class, $collection);
-     }
+    /** @test */
+    public function collection_is_instance_of_iterator_aggregate()
+    {
+        $array = ['one','two'];
+        $collection = new Collection($array);
+        $this->assertInstanceOf(IteratorAggregate::class, $collection);
+    }
+
+    /** @test */
+    public function collection_can_be_iterated()
+    {
+        $collection = new Collection([
+            'one', 'two', 'three'
+        ]);
+
+        $items = [];
+
+        foreach($collection as $item){
+            $items[] = $item;
+        }
+
+        $this->assertCount(3, $items);
+        $this->assertInstanceOf(ArrayIterator::class, $collection->getIterator());
+    }
+
+    /** @test */
+    public function collection_can_be_merged_with_another_collection()
+    {
+        $collection1 = new Collection(['one', 'two']);
+        $collection2 = new Collection(['three', 'four', 'five']);
+
+        $collection1->merge($collection2);
+        
+        $this->assertCount(5, $collection1->get());
+        $this->assertEquals(5, $collection1->count());
+    }
+
+    /** @test */
+    public function can_add_to_existing_collection()
+    {
+        $collection = new Collection(['one', 'two']);
+        $collection->add(['three']);
+
+        $this->assertEquals(3, $collection->count());
+        $this->assertCount(3, $collection->get());
+    }
 }
