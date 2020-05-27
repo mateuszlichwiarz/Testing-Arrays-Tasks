0de9e2df2cd724b8f2639acaeeb937de8b91ce82
Mateusz Lichwiarz <mateusz.lichwiarz@gmail.com>
2020-05-27 14:54:21 +0200

    test, removed zero operands

--- tests/unit/calculator/DivisionTest.php
+++ tests/unit/calculator/DivisionTest.php
@@ -14,4 +14,15 @@ class DivisionTest extends TestCase
 
         $this->assertEquals(50, $division->calculate());
     }
+
+    /** @test */
+    public function removed_devision_by_zero_operands()
+    {
+        $division = new Division();
+        $division->setOperands([10, 0, 0, 5, 0]);
+
+        // 10/5 = 2
+
+        $this->assertEquals(2, $division->calculate());
+    }
 }
