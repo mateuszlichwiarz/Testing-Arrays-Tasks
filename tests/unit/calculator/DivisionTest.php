c9b96a2c44a682f681f2b4aa3dbacbb2afe931da
Mateusz Lichwiarz <mateusz.lichwiarz@gmail.com>
2020-05-27 14:26:47 +0200

    test calculate in division

--- tests/unit/calculator/DivisionTest.php
+++ tests/unit/calculator/DivisionTest.php
@@ -2,7 +2,16 @@
 
 use PHPUnit\Framework\TestCase;
 
+use App\Calculator\Division;
+
 class DivisionTest extends TestCase
 {
-    
+    /** @test */
+    public function divides_given_operands()
+    {
+        $division = new Division();
+        $division->setOperands([100, 2]);
+
+        $this->assertEquals(50, $division->calculate());
+    }
 }
