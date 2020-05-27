ceff51215d3f0515d90142078627fa1c33d1e59e
Mateusz Lichwiarz <mateusz.lichwiarz@gmail.com>
2020-05-27 14:33:37 +0200

    first implement calculate - works

--- app/Calculator/Division.php
+++ app/Calculator/Division.php
@@ -2,7 +2,36 @@
 
 namespace App\Calculator;
 
-class Division
+use App\Calculator\Exceptions\NoOperandsException;
+
+class Division implements OperationInterface
 {
-    
+    protected $operands = [];
+
+    public function setOperands(array $operands)
+    {
+        $this->operands = $operands;
+    }
+
+    public function calculate()
+    {
+        if(count($this->operands) === 0)
+        {
+            throw new NoOperandsException;
+        }
+
+        $result = 0;
+
+        foreach($this->operands as $index => $operand)
+        {
+            if($index === 0)
+            {
+                $result = $operand;
+                continue;
+            }
+
+            $result = $result /$operand;
+        }
+        return $result;
+    }
 }
