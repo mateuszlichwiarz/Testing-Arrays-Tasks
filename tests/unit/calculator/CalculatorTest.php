dfa8444415cc287a031a316e169b3681ff811857
Mateusz Lichwiarz <mateusz.lichwiarz@gmail.com>
2020-05-27 18:57:33 +0200

    test single operation in calculator

--- tests/unit/calculator/CalculatorTest.php
+++ tests/unit/calculator/CalculatorTest.php
@@ -2,7 +2,21 @@
 
 use PHPUnit\Framework\TestCase;
 
+use App\Calculator\Addition;
+//use App\Calculator\Division;
+use App\Calculator\Calculator;
+
 class CalculatorTest extends TestCase
 {
-    
+    /** @test */
+    public function can_set_single_operation()
+    {
+        $addition = new Addition();
+        $addition->setOperands([5, 10]);
+
+        $calculator = new Calculator();
+        $calculator->setOperation($addition);
+
+        $this->assertEqual(1, $calcualtor->getOperations());
+    }
 }
