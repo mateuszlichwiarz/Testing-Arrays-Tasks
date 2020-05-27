683433081e2ab70f3c4ca9e3e495a0c19c367952
Mateusz Lichwiarz <mateusz.lichwiarz@gmail.com>
2020-05-27 19:24:23 +0200

    my own test

--- tests/unit/calculator/CalculatorTest.php
+++ tests/unit/calculator/CalculatorTest.php
@@ -3,7 +3,7 @@
 use PHPUnit\Framework\TestCase;
 
 use App\Calculator\Addition;
-//use App\Calculator\Division;
+use App\Calculator\Division;
 use App\Calculator\Calculator;
 
 class CalculatorTest extends TestCase
@@ -33,5 +33,24 @@ class CalculatorTest extends TestCase
         $calculator->setOperations([$addition1, $addition2]);
 
         $this->assertCount(2, $calculator->getOperations());
+    }
+
+    /** @test */
+    public function my_own_test_really_addition_and_devision_multiple_operations()
+    {
+        
+        $addition1 = new Addition();
+        $addition1->setOperands([5, 10]);
+
+        $addition2 = new Addition();
+        $addition2->setOperands([2, 2]);
+
+        $devision = new Division();
+        $devision->setOperands([4, 2]);
+
+        $calculator = new Calculator();
+        $calculator->setOperations([$addition1, $addition2, $devision]);
+
+        $this->assertCount(3, $calculator->getOperations());
     }
 }
