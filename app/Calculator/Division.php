c21b77cdf8dcac5e2236f91895c3eccc3c9f2ae6
Mateusz Lichwiarz <mateusz.lichwiarz@gmail.com>
2020-05-27 14:41:44 +0200

    refactoring calculator, add operationAbstract class

--- app/Calculator/Division.php
+++ app/Calculator/Division.php
@@ -4,15 +4,8 @@ namespace App\Calculator;
 
 use App\Calculator\Exceptions\NoOperandsException;
 
-class Division implements OperationInterface
+class Division extends OperationAbstract implements OperationInterface
 {
-    protected $operands = [];
-
-    public function setOperands(array $operands)
-    {
-        $this->operands = $operands;
-    }
-
     public function calculate()
     {
         if(count($this->operands) === 0)
---
+++ app/Calculator/OperationAbstract.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Calculator;
+
+abstract class OperationAbstract
+{
+    protected $operands = [];
+
+    public function setOperands(array $operands)
+    {
+        $this->operands = $operands;
+    }
+
+}
