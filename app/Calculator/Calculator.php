54921855cab64e7cad01e3339965db974b97603b
Mateusz Lichwiarz <mateusz.lichwiarz@gmail.com>
2020-05-27 19:12:11 +0200

    enter setOperation to calculator

--- app/Calculator/Calculator.php
+++ app/Calculator/Calculator.php
@@ -4,5 +4,15 @@ namespace App\Calculator;
 
 class Calculator
 {
-    
+    protected $operations = [];
+
+    public function setOperation(OperationInterface $operation)
+    {
+        $this->operations[] = $operation;
+    }
+
+    public function getOperations()
+    {
+        return $this->operations;
+    }
 }
