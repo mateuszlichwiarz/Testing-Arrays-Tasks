20f543c753fad2b4242a502c02688b681e5f3039
Mateusz Lichwiarz <mateusz.lichwiarz@gmail.com>
2020-05-28 21:53:54 +0200

    ksort - sort only key, not value

--- app/arrays/arrays.php
+++ app/arrays/arrays.php
@@ -1,9 +1,9 @@
 <?php
 
 
-$names   = ['alex', 'dale', 'billy', 'ashley'];
+$names   = ['alex' => 26, 'dale'=> 1, 'billy' => 3, 'ashley' => 56];
 $numbers = [1,3,6,1,2,6,4,7,8,10,3,5];
 
-asort($names);
+krsort($names);
 
 var_dump($names);
