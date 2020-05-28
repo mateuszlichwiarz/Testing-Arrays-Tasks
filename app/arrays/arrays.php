534a5d2ecbee412d803a806636a5a86a936b7073
Mateusz Lichwiarz <mateusz.lichwiarz@gmail.com>
2020-05-28 21:58:20 +0200

    natsort - very unusual, in some cases powerfull sorting

--- app/arrays/arrays.php
+++ app/arrays/arrays.php
@@ -1,9 +1,8 @@
 <?php
 
 
-$names   = ['alex' => 26, 'dale'=> 1, 'billy' => 3, 'ashley' => 56];
-$numbers = [1,3,6,1,2,6,4,7,8,10,3,5];
+$files = ['img12.png', 'img10.png', 'img2.png', 'img1,png'];
 
-krsort($names);
+natsort($files);
 
-var_dump($names);
+var_dump($files);
