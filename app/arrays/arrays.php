1d2278b149c8aa1dd816eb747065fe9565e658d4
Mateusz Lichwiarz <mateusz.lichwiarz@gmail.com>
2020-05-28 23:33:16 +0200

    more complex example how usort works

--- app/arrays/arrays.php
+++ app/arrays/arrays.php
@@ -1,9 +1,25 @@
 <?php
 
-$numbers = [4, 4, 10, 1, 52, 89];
+$users = [
+    ['username' => 'alex', 'reputation' => 50, 'posts' => 200],
+    ['username' => 'dale', 'reputation' => 300, 'posts' => 150],
+    ['username' => 'billy', 'reputation' => 2, 'posts' => 5],
+    ['username' => 'ashley', 'reputation' => 1, 'posts' => 6],
+];
 
-usort($numbers, function($first, $second) {
-    return $first <=> $second;
-});
+function sortByScore($first, $second)
+{
+    $scoreOne = $first['reputation'] = $first['posts'];
+    $scoreTwo = $second['reputation'] = $second['posts'];
 
-var_dump($numbers);
+    if($scoreOne === $scoreTwo)
+    {
+        return ($first['reputation'] > $second['reputation']) ? 0 : 1;
+    }
+
+    return ($scoreOne > $scoreTwo) ? -1 : 1;
+}
+
+usort($users, 'sortByScore');
+
+var_dump($users);
