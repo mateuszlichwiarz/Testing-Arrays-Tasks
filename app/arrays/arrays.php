8775065160b8b25a2be608576d9353068e8f4039
Mateusz Lichwiarz <mateusz.lichwiarz@gmail.com>
2020-05-28 23:40:25 +0200

    array_multisort - finally, i understood this method

--- app/arrays/arrays.php
+++ app/arrays/arrays.php
@@ -1,25 +1,7 @@
 <?php
 
-$users = [
-    ['username' => 'alex', 'reputation' => 50, 'posts' => 200],
-    ['username' => 'dale', 'reputation' => 300, 'posts' => 150],
-    ['username' => 'billy', 'reputation' => 2, 'posts' => 5],
-    ['username' => 'ashley', 'reputation' => 1, 'posts' => 6],
-];
+$file = ['img12.png', 'img10.png', 'img2.png', 'img1.png'];
 
-function sortByScore($first, $second)
-{
-    $scoreOne = $first['reputation'] = $first['posts'];
-    $scoreTwo = $second['reputation'] = $second['posts'];
+array_multisort($file, SORT_ASC, SORT_NATURAL);
 
-    if($scoreOne === $scoreTwo)
-    {
-        return ($first['reputation'] > $second['reputation']) ? 0 : 1;
-    }
-
-    return ($scoreOne > $scoreTwo) ? -1 : 1;
-}
-
-usort($users, 'sortByScore');
-
-var_dump($users);
+var_dump($file);
