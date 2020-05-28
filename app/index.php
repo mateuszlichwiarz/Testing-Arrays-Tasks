d7ccc5cb3f43ad4111c72e5f3b6bf54e9cd53bf4
Mateusz Lichwiarz <mateusz.lichwiarz@gmail.com>
2020-05-29 00:21:47 +0200

    much easer if's

--- app/index.php
+++ app/index.php
@@ -1,2 +1,33 @@
 <?php
 
+//
+/*
+if(isset($_POST['file']))
+{
+    if(in_array($fileExtension, $allowedFileExtension))
+    {
+        //upload file;
+    }else {
+        return;
+        //error, redirect
+    }
+} else {
+    return;
+    //error, redirect
+}
+*/
+
+if(!isset($_POST['file']))
+{
+    return;
+    //error, redirect
+}
+
+if(!in_array($fileExtension, $allowedFileExtension))
+{
+    return;
+
+    //error, redirect
+}
+
+// Upload the file!
