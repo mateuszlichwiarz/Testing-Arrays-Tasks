0f538a62919d507238d34d8924c10b8e97b1c4b6
Mateusz Lichwiarz <mateusz.lichwiarz@gmail.com>
2020-05-28 22:34:24 +0200

    <=> - nice way to shorter code

--- app/arrays/arrays.php
+++ app/arrays/arrays.php
@@ -3,12 +3,7 @@
 $numbers = [4, 4, 10, 1, 52, 89];
 
 usort($numbers, function($first, $second) {
-    if($first === $second)
-    {
-        return 0;
-    }
-
-    return($first > $second) ? 1 : -1;
+    return $first <=> $second;
 });
 
 var_dump($numbers);
