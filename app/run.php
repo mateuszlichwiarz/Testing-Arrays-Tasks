710b78fbc83f7a0cd849bd9b675c0772ba3892a7
Mateusz Lichwiarz <mateusz.lichwiarz@gmail.com>
2020-06-18 21:02:47 +0200

    my problem with autoload solved :D

--- app/run.php
+++ app/run.php
@@ -1,14 +1,16 @@
-<?php
+<?php 
 
 namespace App;
+
+require_once '../vendor/autoload.php';
 
 use App\employee\NastyBoss;
 use App\employee\Minion;
 use App\employee\CluedUp;
 
-require_once 'employee/NastyBoss.php';
-require_once 'employee/Minion.php';
-require_once 'employee/CluedUp.php';
+//require_once 'employee/NastyBoss.php';
+//require_once 'employee/Minion.php';
+//require_once 'employee/CluedUp.php';
 
 $boss = new NastyBoss();
 $boss->addEmployee(new Minion("Mateusz"));
--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <?php
 
+require_once 'vendor/autoload.php';
 require_once 'app/start.php';
 
 $user = new User();
