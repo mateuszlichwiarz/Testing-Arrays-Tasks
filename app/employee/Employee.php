7cda19b4d4be240062e48c126e4e5771b9cc8989
Mateusz Lichwiarz <mateusz.lichwiarz@gmail.com>
2020-06-18 22:54:46 +0200

    static function to instantiation object type Employee

--- app/employee/Employee.php
+++ app/employee/Employee.php
@@ -5,6 +5,15 @@ namespace App\employee;
 abstract class Employee
 {
     protected $name;
+    private static $types = ['Minion', 'CluedUp', 'WellConected'];
+
+    public static function recruit($name)
+    {
+        $num = rand(1, count(self::$types)) - 1;
+        $class = __NAMESPACE__ . '\\'.self::$types[$num];
+        
+        return new $class($name);
+    }
 
     public function __construct(string $name)
     {
