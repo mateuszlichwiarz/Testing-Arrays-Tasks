27baf52f6ec734cc4de75bdd780ef4068f0be441
Mateusz Lichwiarz <mateusz.lichwiarz@gmail.com>
2020-06-23 23:52:23 +0200

    instantiation realized static

--- app/preferences/Preferences.php
+++ app/preferences/Preferences.php
@@ -5,8 +5,19 @@ namespace App\Preferences;
 class Preferences
 {
     private $props = [];
+    private static $instance;
 
     private function __construct(){}
+
+    public static function getInstance() 
+    {
+        if(empty(self::$instance))
+        {
+            self::$instance = new Preferences();
+        }
+
+        return self::$instance;
+    } 
 
     public function setProperty(string $key, string $val)
     {
