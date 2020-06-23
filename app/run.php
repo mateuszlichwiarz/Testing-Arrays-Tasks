07c855884cad8e960a5b99fdb2f835f25db4bd9c
Mateusz Lichwiarz <mateusz.lichwiarz@gmail.com>
2020-06-23 23:58:11 +0200

    empty run

--- app/run.php
+++ app/run.php
@@ -4,12 +4,12 @@ namespace App;
 
 require_once '../vendor/autoload.php';
 
-use App\employee\NastyBoss;
-use App\employee\Minion;
-use App\employee\CluedUp;
-use App\employee\WellConnected;
-use App\employee\Employee;
+//use App\employee\NastyBoss;
+//use App\employee\Minion;
+//use App\employee\CluedUp;
+//use App\employee\WellConnected;
+//use App\employee\Employee;
 
-$boss = new NastyBoss();
-$boss->addEmployee(Employee::recruit("Mateusz"));
-$boss->projectFails();
+//$boss = new NastyBoss();
+//$boss->addEmployee(Employee::recruit("Mateusz"));
+//$boss->projectFails();
