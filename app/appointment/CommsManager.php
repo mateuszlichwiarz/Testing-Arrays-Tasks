71b494f394ca59513762e12d3a35855e77161e7a
Mateusz Lichwiarz <mateusz.lichwiarz@gmail.com>
2020-06-25 00:06:06 +0200

    handling few types of ApptEncoder in CommsManager

--- app/appointment/CommsManager.php
+++ app/appointment/CommsManager.php
@@ -4,8 +4,32 @@ namespace App\appointment;
 
 class CommsManager
 {
+    const  BLOGGS = 1;
+    const    MEGA = 2;
+    private $mode = 1;
+
+    public function __construct(int $mode)
+    {
+        $this->mode = $mode;
+    }
+    
     public function getApptEncoder(): ApptEncoder
     {
-        return new BloggsApptEncoder();
+        switch($this->mode){
+            case (self::MEGA):
+                return new MegaApptEncoder();
+            default:
+                return new BloggsApptEncoder();
+        }
+    }
+
+    public function getHeaderText(): string
+    {
+        switch($this->mode){
+            case (self::MEGA):
+                return "Header MegaCal\n";
+            default:
+                return "Header BloggsCal\n";
+        }
     }
 }
