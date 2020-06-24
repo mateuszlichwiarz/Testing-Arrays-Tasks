e904bac95948a1fadb5f573328608105cb8241ff
Mateusz Lichwiarz <mateusz.lichwiarz@gmail.com>
2020-06-25 01:32:02 +0200

    CommsManager abstract class

--- app/appointment/CommsManager.php
+++ app/appointment/CommsManager.php
@@ -6,34 +6,9 @@ use App\appointment\ApptEncoder;
 use App\appointment\BloggsApptEncoder;
 use App\appointment\MegaApptEncoder;
 
-class CommsManager
+abstract class CommsManager
 {
-    const  BLOGGS = 1;
-    const    MEGA = 2;
-    private $mode = 1;
-
-    public function __construct(int $mode)
-    {
-        $this->mode = $mode;
-    }
-    
-    public function getApptEncoder(): ApptEncoder
-    {
-        switch($this->mode){
-            case (self::MEGA):
-                return new MegaApptEncoder();
-            default:
-                return new BloggsApptEncoder();
-        }
-    }
-
-    public function getHeaderText(): string
-    {
-        switch($this->mode){
-            case (self::MEGA):
-                return "Header MegaCal\n";
-            default:
-                return "Header BloggsCal\n";
-        }
-    }
+    abstract public function getApptEncoder(): ApptEncoder;
+    abstract public function getHeaderText(): string;
+    abstract public function getFooterText(): string;
 }
